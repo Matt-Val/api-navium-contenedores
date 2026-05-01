056825ca7d934c88d29d5f53b6dc6630eb5b1a98
Matt-Val <mat.ramosv@duocuc.cl>
2026-04-30 21:37:08 -0400

    Add: Implementación de Librería de Seguridad

--- src/main/java/com/navium/contenedores/Application.java
+++ src/main/java/com/navium/contenedores/Application.java
@@ -2,8 +2,10 @@ package com.navium.contenedores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.navium")
 public class Application {
 
 	public static void main(String[] args) {
